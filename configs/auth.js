72efd285ca9c1318a56888adab6d3d12413e39eb
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-04-19 10:48:52 +0200

    add config vars

--- configs/auth.js
+++ configs/auth.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: `${process.env.CLIENT_ID}`,
     clientSecret: `${process.env.CLIENT_SECRET}`,
-    callbackURL: `${process.env.HEROKU}/auth/google/callBack`,
+    callbackURL: `${process.env.URL_HEROKU}/google/callBack`,
     proxy: true 
   },
   function(request, accessToken, refreshToken, profile, done) {
