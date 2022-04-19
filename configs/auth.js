c77eab87830e2640a41c4c7bde2e1d6121e829d4
Alenriquez96 <alberto.enriquez96@gmail.com>
2022-04-19 09:44:33 +0200

    testing auth

--- configs/auth.js
+++ configs/auth.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: `${process.env.CLIENT_ID}`,
     clientSecret: `${process.env.CLIENT_SECRET}`,
-    callbackURL: `${process.env.URL_BASE}/google/callBack`
+    callbackURL: `${process.env.HEROKU}/google/callBack`
   },
   function(request, accessToken, refreshToken, profile, done) {
     return done(null, profile);
