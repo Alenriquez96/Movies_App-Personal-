c07bf55c958e74658b2dfa7943c6892c3a4ac764
alenriquez96 <alberto.enriquez96@gmail.com>
2022-04-20 14:54:53 +0200

    spinner 2

--- public/js/spinner.js
+++ public/js/spinner.js
@@ -5,15 +5,11 @@ const a = document.querySelector(".toDetails")
 if (form) {
     form.addEventListener("submit", () => {
         spinner.style.display = "inline-block";
-        spinner.style.flexDirecton = "center";
-        spinner.style.alignItems = "center";
     })
 }
 
 if (a) {
     a.addEventListener("click", () => {
-        spinner.style.display = "flex";
-        spinner.style.flexDirecton = "center";
-        spinner.style.alignItems = "center";
+        spinner.style.display = "inline-block";
     })
 }
